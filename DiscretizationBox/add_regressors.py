94472632df554bbaeebbd10a02e12b6f69eed0e9
Giovanni3A <giovanni3amorim@gmail.com>
2021-01-28 12:03:28 -0300

    feature update: fix functions bugs and add ID column as parameter

--- DiscretizationBox/add_regressors.py
+++ DiscretizationBox/add_regressors.py
@@ -1,44 +1,25 @@
 import os
-
 import pandas as pd
 import geopandas as gpd
 
-sys.path.append('..')
-from config import *
-
-# - file inputs example
-regressor_area = gpd.read_file(os.path.join(ENTR_DATA_PATH,'Bairros_rio_de_janeiro.shp'))
-regressor_area = regressor_area.set_crs(epsg=29183)
-regressor_area = regressor_area.to_crs(epsg=4326)
-regressor = regressor_area[['area','geometry']]
-regressor.head()
-
-df = gpd.read_file(os.path.join(ENTR_DATA_PATH,'sh','sh.shp'))
-df.rename({'h3_index':'discr_id'},axis=1,inplace=True)
-discretization = df[['discr_id','geometry']].copy()
-
-def addRegressorWeightedAverage(df : gpd.GeoDataFrame, regressor_df : gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-
-
+def addRegressorWeightedAverage(df : gpd.GeoDataFrame, regressor_df : gpd.GeoDataFrame, discr_id_col : str = 'h3_index') -> gpd.GeoDataFrame:
     '''
     Apply parameters from regressor database to geographic discretization as a wieghted average of the areas of intersection.
-
     Parameters
         df : gpd.GeoDataFrame - geodataframe containing the final geographic discretization and a columns 'discr_id' containing unique IDs.
         regressor_df : gpd.GeoDataFrame - geodataframe containing the desired regressors and geometries defining geographic boundaries.
-
+        discr_id_col : str - ID column from df
     Returns
         overlay_df : gpd.GeoDataFrame - geodataframe with same structure as df with new columns of applied regressors.
-
     '''
 
     # merge calculating area overlays
-    overlay_df = gpd.overlay(discretization,regressor,how='intersection').rename({'geometry':'geometry_overlay'},axis=1)
+    overlay_df = gpd.overlay(df,regressor_df,how='intersection').rename({'geometry':'geometry_overlay'},axis=1)
     # merge with df to get original discretization
     overlay_df = pd.merge(
         overlay_df,
         df,
-        on='discr_id',
+        on=discr_id_col,
         how='outer'
     ).rename({'geometry':'geometry_discr'},axis=1)
 
@@ -54,33 +35,28 @@ def addRegressorWeightedAverage(df : gpd.GeoDataFrame, regressor_df : gpd.GeoDat
     for regressor_col in regressor_cols:
         overlay_df[regressor_col] *= overlay_df['overlay_area_percentage']
 
-    # sum weithed partials and recover original discretization geometries
-    discr_geometries = df.set_index('discr_id')['geometry']
-    overlay_df = overlay_df.groupby(['discr_id'])[regressor_cols].sum()
-    overlay_df['geometry'] = discr_geometries
+    # sum weithed partials and recover original columns
+    overlay_df = overlay_df.groupby([discr_id_col])[regressor_cols].sum()
+    overlay_df = pd.merge(df,overlay_df.reset_index(),on=discr_id_col,how='left')
 
-    return overlay_df.reset_index()
+    return overlay_df
 
-def addRegressorUniformDistribution(df : gpd.GeoDataFrame, regressor_df : gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-
-
+def addRegressorUniformDistribution(df : gpd.GeoDataFrame, regressor_df : gpd.GeoDataFrame, discr_id_col : str = 'h3_index') -> gpd.GeoDataFrame:
     '''
     Apply parameters from regressor database to geographic discretization as a uniform distribution of parameters in regressors area.
-
     Parameters
         df : gpd.GeoDataFrame - geodataframe containing the final geographic discretization and a columns 'discr_id' containing unique IDs.
         regressor_df : gpd.GeoDataFrame - geodataframe containing the desired regressors and geometries defining geographic boundaries.
-
+        discr_id_col : str - discretization ID column from df
     Returns
         overlay_df : gpd.GeoDataFrame - geodataframe with same structure as df with new columns of applied regressors.
-
     '''
 
     # create regressors ID column
     regressor_df['regr_id'] = list(range(len(regressor_df)))
 
     # merge calculating area overlays
-    overlay_df = gpd.overlay(discretization,regressor,how='intersection').rename({'geometry':'geometry_overlay'},axis=1)
+    overlay_df = gpd.overlay(df,regressor_df,how='intersection').rename({'geometry':'geometry_overlay'},axis=1)
     # merge with df to get original discretization
     overlay_df = pd.merge(
         overlay_df,
@@ -97,11 +73,8 @@ def addRegressorUniformDistribution(df : gpd.GeoDataFrame, regressor_df : gpd.Ge
     for regressor_col in regressor_cols:
         overlay_df[regressor_col] *= overlay_df['overlay_area_percentage']
 
-    # sum weithed partials and recover original discretization geometries
-    discr_geometries = df.set_index('discr_id')['geometry']
-    overlay_df = overlay_df.groupby(['discr_id'])[regressor_cols].sum()
-    overlay_df['geometry'] = discr_geometries
-    
-    return overlay_df.reset_index()
+    # sum weithed partials and recover original columns
+    overlay_df = overlay_df.groupby([discr_id_col])[regressor_cols].sum()
+    overlay_df = pd.merge(df,overlay_df.reset_index(),on=discr_id_col,how='left')
 
-r2 = addRegressorUniformDistribution(discretization,regressor)
+    return overlay_df
