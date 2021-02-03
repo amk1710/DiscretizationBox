872a975ec11b43649736f11732021351f0910d63
André Mazal Krauss <amk1710@gmail.com>
2021-02-03 12:16:14 -0300

    add aditional protection against column types

--- DiscretizationBox/add_regressors.py
+++ DiscretizationBox/add_regressors.py
@@ -4,14 +4,14 @@ import geopandas as gpd
 import numbers
 from warnings import warn
 
-def addRegressorWeightedAverage(df : gpd.GeoDataFrame, regressor_df : gpd.GeoDataFrame, discr_id_col : str = 'h3_index', regressor_cols = None) -> gpd.GeoDataFrame:
+def addRegressorWeightedAverage(df : gpd.GeoDataFrame, regressor_df : gpd.GeoDataFrame, discr_id_col : str = 'h3_index', regressor_columns = None) -> gpd.GeoDataFrame:
     '''
     Apply parameters from regressor database to geographic discretization as a wieghted average of the areas of intersection.
     Parameters
         df : gpd.GeoDataFrame - geodataframe containing the final geographic discretization and a columns 'discr_id' containing unique IDs.
         regressor_df : gpd.GeoDataFrame - geodataframe containing the desired regressors and geometries defining geographic boundaries.
         discr_id_col : str - ID column from df
-        regressor_cols : list - list of column names that should be considered
+        regressor_columns : list - list of column names that should be considered
     Returns
         overlay_df : gpd.GeoDataFrame - geodataframe with same structure as df with new columns of applied regressors.
     '''
@@ -34,28 +34,33 @@ def addRegressorWeightedAverage(df : gpd.GeoDataFrame, regressor_df : gpd.GeoDat
     overlay_df['overlay_area_percentage'] = overlay_df['geometry_overlay_area'] / overlay_df['geometry_discr'].area
 
     # apply regressor columns multiplying merged values with calculated percentages
-    if regressor_cols is None:
-        regressor_cols = [col for col in regressor_df.columns if not(col in ['geometry','regr_id'])]
-    for regressor_col in regressor_cols:
-        if issubclass(overlay_df[regressor_col].dtype.type, numbers.Number):
-            overlay_df[regressor_col] *= overlay_df['overlay_area_percentage']
-        else:
-            warn('Column ' + str(regressor_col) + 'isn\'t numeric. Regressor won\'t be applied')
+    if regressor_columns is None:
+        regressor_columns = [col for col in regressor_df.columns if not(col in ['geometry','regr_id'])]
+    for col in regressor_columns:
+        try:
+            if issubclass(overlay_df[col].dtype.type, numbers.Number):
+                overlay_df[col] *= overlay_df['overlay_area_percentage']
+            else:
+                warn('Column ' + str(col) + 'isn\'t numeric. Regressor won\'t be applied')
+        except Error:
+            warn('Column ' + str(col) + 'was ignored because an error ocurred')
 
+    #remove any columns that may have been skipped:
+    regressor_columns = [col for col in regressor_columns if col in overlay_df.columns]
     # sum weithed partials and recover original columns
-    overlay_df = overlay_df.groupby([discr_id_col])[regressor_cols].sum()
+    overlay_df = overlay_df.groupby([discr_id_col])[regressor_columns].sum()
     overlay_df = pd.merge(df,overlay_df.reset_index(),on=discr_id_col,how='left')
 
     return overlay_df
 
-def addRegressorUniformDistribution(df : gpd.GeoDataFrame, regressor_df : gpd.GeoDataFrame, discr_id_col : str = 'h3_index', regressor_cols = None) -> gpd.GeoDataFrame:
+def addRegressorUniformDistribution(df : gpd.GeoDataFrame, regressor_df : gpd.GeoDataFrame, discr_id_col : str = 'h3_index', regressor_columns = None) -> gpd.GeoDataFrame:
     '''
     Apply parameters from regressor database to geographic discretization as a uniform distribution of parameters in regressors area.
     Parameters
         df : gpd.GeoDataFrame - geodataframe containing the final geographic discretization and a columns 'discr_id' containing unique IDs.
         regressor_df : gpd.GeoDataFrame - geodataframe containing the desired regressors and geometries defining geographic boundaries.
         discr_id_col : str - discretization ID column from df
-        regressor_cols : list - list of column names that should be considered
+        regressor_columns : list - list of column names that should be considered
     Returns
         overlay_df : gpd.GeoDataFrame - geodataframe with same structure as df with new columns of applied regressors.
     '''
@@ -77,16 +82,21 @@ def addRegressorUniformDistribution(df : gpd.GeoDataFrame, regressor_df : gpd.Ge
     overlay_df['overlay_area_percentage'] = overlay_df['geometry_overlay'].area / overlay_df['geometry_regr'].area
 
     # apply regressor columns multiplying merged values with calculated percentages
-    if regressor_cols is None:
-        regressor_cols = [col for col in regressor_df.columns if not(col in ['geometry','regr_id'])]
-    for regressor_col in regressor_cols:
-        if issubclass(overlay_df[regressor_col].dtype.type, numbers.Number):
-            overlay_df[regressor_col] *= overlay_df['overlay_area_percentage']
-        else:
-            warn('Column ' + str(regressor_col) + 'isn\'t numeric. Regressor won\'t be applied')
+    if regressor_columns is None:
+        regressor_columns = [col for col in regressor_df.columns if not(col in ['geometry','regr_id'])]
+    for col in regressor_columns:
+        try:
+            if issubclass(overlay_df[col].dtype.type, numbers.Number):
+                overlay_df[col] *= overlay_df['overlay_area_percentage']
+            else:
+                warn('Column ' + str(col) + 'isn\'t numeric. Regressor won\'t be applied')
+        except:
+            warn('Column ' + str(col) + 'was ignored because an error ocurred')
 
+    #remove any columns that may have been skipped:
+    regressor_columns = [col for col in regressor_columns if col in overlay_df.columns]
     # sum weithed partials and recover original columns
-    overlay_df = overlay_df.groupby([discr_id_col])[regressor_cols].sum()
+    overlay_df = overlay_df.groupby([discr_id_col])[regressor_columns].sum()
     overlay_df = pd.merge(df,overlay_df.reset_index(),on=discr_id_col,how='left')
 
     return overlay_df
