c6e568ee867720ccd870ee849044a875fed00b09
Giovanni3A <giovanni3amorim@gmail.com>
2021-02-03 16:06:33 -0300

    adds setup script for building DiscretizationBox package

---
+++ setup.py
@@ -0,0 +1,20 @@
+####### TODO
+
+import setuptools
+
+setuptools.setup(
+    name='dbox',
+    version='0',
+    author='André Mazal Krauss, Giovanni Amorim',
+    # maintainer_email='',
+    # url='link do devops/github',
+    # license="",
+    description='Python package "Discretization Box" used to geographic segmentation',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 0 - V0',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.9',
+        # 'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+)
